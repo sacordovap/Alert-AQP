ccc1eac0ffa1132154ed2ea7610d02a2846efb3c
ccahui <kris_ti4n@hotmail.com>
2021-11-09 17:35:01 -0500

    Archivos de librerias necesarias para el proyecto

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ky)%$g-opwib$_f8^d#q5b5r8#24ql!t_^oy1&x*k9&lo%!1^$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
